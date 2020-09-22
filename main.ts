026b2f0866afe59d52272dc039045912ac66b113
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-09-22 14:19:54 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(100)
+        basic.clearScreen()
+        basic.pause(100)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(100)
+        basic.clearScreen()
+        basic.pause(100)
+    }
 })
